077b36f89e72951a5555b25e3ea5e3b2c0dc0439
Gema Refantero <refara.08@gmail.com>
2022-08-24 10:57:31 +0700

    testing: removing hero section

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home(props) {
 
   return (
     <div>
-      <Hero />
+      {/* <Hero /> */}
       <Services />
       <Skills icons={allIcons} />
       <Portfolio portfolioList={props.posts} />
