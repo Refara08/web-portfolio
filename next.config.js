71928e9f20fb50802da670cde4a1f7078467c590
Gema Refantero <refara.08@gmail.com>
2022-08-28 20:51:52 +0700

    revised environment variables

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        reactStrictMode: true,
-        swcMinify: true,
-        mongodb_username: "refara08",
-        mongodb_password: "Angrybird08",
-        mongodb_cluster: "cluster0",
-        mongodb_database: "portfolio-contact-dev",
-      },
-    };
-  }
-
-  return {
-    env: {
-      reactStrictMode: true,
-      swcMinify: true,
-      mongodb_username: "refara08",
-      mongodb_password: "Angrybird08",
-      mongodb_cluster: "cluster0",
-      mongodb_database: "portfolio-contact",
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
