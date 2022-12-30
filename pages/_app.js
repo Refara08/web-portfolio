6c5493ba84c216b0967e39400e108895fa1f4870
Gema Refantero <96099133+Refara08@users.noreply.github.com>
2022-12-30 10:00:06 +0700

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,24 @@ function MyApp({ Component, pageProps }) {
           />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="mobile-web-app-capable" content="yes" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <Component {...pageProps} />
       </Layout>
